3034e819cd5c3890038f9ee17b3b5cc26ca3a8c8
Chaitanya Asawa <casawa@stanford.edu>
2020-03-22 02:59:48 -0700

    Refactor app setup

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ import Simulation from './sim.js'
 const population_size = 10 * 1000
 const r0 = 2
 const initial_outbreak_size = 3
-const conditionsElem = document.getElementById("initConditions")
-conditionsElem.innerHTML = `Population Size: ${population_size}, Initial Outbreak Size: ${initial_outbreak_size}`
 
 let sim = new Simulation(population_size, r0, initial_outbreak_size)
 let day = 0
-
-const statsTextElem = document.getElementById("stats")
 
 function display_stats(display_unknown) {
     // Admittedly slightly jank, could instead append child nodes/etc
@@ -24,24 +20,15 @@ function display_stats(display_unknown) {
     }
     stats_text = stats_text.concat(`<p>Number recovered: ${sim.num_recovered}</p>`)
     stats_text = stats_text.concat(`<p>Number of deaths: ${sim.num_deaths}</p>`)
-    statsTextElem.innerHTML = stats_text
-}
-
-const slider = document.getElementById("distLikelihoodSlider")
-const sliderValue = document.getElementById("sliderVal")
-sliderValue.innerHTML = slider.value;
-slider.oninput = function() {
-  sliderValue.innerHTML = this.value;
+    document.getElementById("stats").innerHTML = stats_text
 }
 
 function run_step() {
+    const slider = document.getElementById("distLikelihoodSlider")
     sim.step_day(slider.value / 100.);
     day += 1
     display_stats(false)
 }
-
-const runButton = document.getElementById("runButton")
-runButton.onclick = run_step
 
 function reset() {
     day = 0
@@ -49,7 +36,24 @@ function reset() {
     display_stats(false)
 }
 
-const resetButton = document.getElementById("resetButton")
-resetButton.onclick = reset
+function setup() {
+    const conditionsElem = document.getElementById("initConditions")
+    conditionsElem.innerHTML = `Population Size: ${population_size}, Initial Outbreak Size: ${initial_outbreak_size}`
 
-display_stats(false)
+    const slider = document.getElementById("distLikelihoodSlider")
+    const sliderValue = document.getElementById("sliderVal")
+    sliderValue.innerHTML = slider.value;
+    slider.oninput = function() {
+      sliderValue.innerHTML = this.value;
+    }
+
+    const runButton = document.getElementById("runButton")
+    runButton.onclick = run_step
+
+    const resetButton = document.getElementById("resetButton")
+    resetButton.onclick = reset
+
+    display_stats(false)
+}
+
+setup()
